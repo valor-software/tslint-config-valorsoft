b0b453f98fb68ce71b1b7e04315278c82adf719d
Dmitriy Schekhovtsov <valorkin@gmail.com>
2016-12-02 00:52:38 +0200

    feat(package): updated codelyzer and tslint dependencies

--- test/config.spec.js
+++ test/config.spec.js
@@ -1,3 +1,4 @@
+// skip deprecated
 'use strict';
 
 const fs = require('fs');
@@ -8,6 +9,9 @@ const stripComments = require('strip-json-comments');
 
 const config = JSON.parse(stripComments(readFile('./tslint.json')));
 const rulesInConfig = Object.keys(config.rules);
+
+// todo: hack for deprecated rules
+rulesInConfig.push('no-unused-variable');
 
 const rulesInCodelyzer = fs.readdirSync('./node_modules/codelyzer')
   .filter(fileName => /Rule\.js/.test(fileName))
@@ -25,14 +29,42 @@ describe('valor config rules set', () => {
       .equal(rulesInTslint.length + rulesInCodelyzer.length);
   });
   it('should contain all rules from tslint core', () => {
+    const missingRules = [];
     rulesInTslint.forEach(rule => {
-      expect(rulesInConfig).to.contains(rule);
+      if (rulesInConfig.indexOf(rule) === -1) {
+        missingRules.push(rule);
+      }
     });
+    if (missingRules.length !== 0) {
+      throw new Error(missingRules);
+    }
+  });
+  it('should not contain deprecated rules', () => {
+    const deprecatedRules = [];
+    rulesInConfig.forEach(rule => {
+      if (rulesInTslint.indexOf(rule) === -1 && rulesInCodelyzer.indexOf(rule) === -1) {
+        deprecatedRules.push(rule);
+      }
+    });
+    if (deprecatedRules.length !== 0) {
+      throw new Error(deprecatedRules);
+    }
   });
   it('should contain all rules from codelyzer', () => {
+    const missingRules = [];
     rulesInCodelyzer.forEach(rule => {
-      expect(rulesInConfig).to.contains(rule);
+      // expect(rulesInConfig).to.contains(rule);
+      if (rulesInConfig.indexOf(rule) === -1) {
+        missingRules.push(rule);
+      }
     });
+    if (missingRules.length !== 0) {
+      throw new Error(missingRules);
+    }
+
+    // rulesInCodelyzer.forEach(rule => {
+    //   expect(rulesInConfig).to.contains(rule);
+    // });
   });
 });
 
